6b33d348be0e527bf1e34c9407b176185b4d94b8
Bojan Bakaric <bakaricbojan@gmail.com>
2023-03-21 07:27:17 +0000

    Initial commit

--- arrow-functions.js
+++ arrow-functions.js
@@ -5,10 +5,44 @@
 
 // Vanilla JavaScript Function
 
+function addTwoNumbers1(a, b) {
+    // Code block
+    return a + b;
+}
+
+let sum1 = addTwoNumbers1(3, 5);
+console.log(sum1);
+
 // Arrow Function With Parameters
+
+const addTwoNumbers2= (a, b) => {
+    // Code block
+    return a + b;
+}
+
+let sum2 = addTwoNumbers2(12, 5);
+console.log(sum2);
 
 // Single Line Arrow Function With Parameters
 
+// const addTwoNumbers= (a, b) => (a + b); also valid
+const addTwoNumbers= (a, b) => a + b;
+let sum = addTwoNumbers(1, 5);
+console.log(sum);
+
 // Implicit Returns
 
+const saySomething = message => console.log(message);
+saySomething('Oi there!')
+
+const sayHello = () => console.log('Oiiii')
+sayHello();
+
 // Returning Multiple Lines
+
+const returnMultipleLines = () => (
+    `<p>
+    This is a multiline string!
+    </p>`
+)
+console.log(returnMultipleLines())
